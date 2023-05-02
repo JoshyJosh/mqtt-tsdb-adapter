e01e1a0d042e527a1331a7540153d813572b0a46
joshyjosh <herukula@gmail.com>
2023-05-02 17:31:17 +0200

    add parallel execution to test cases, update test cases for JSON and CSV mqtt parsers

--- app/db/db_test.go
+++ app/db/db_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestCompileTDEngineMetricsAndTags(t *testing.T) {
+	t.Parallel()
 
 	cases := []struct {
 		name            string
--- app/mqtt/mqtt_test.go
+++ app/mqtt/mqtt_test.go
@@ -9,6 +9,8 @@ import (
 )
 
 func TestParseJSON(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		name              string
 		body              []byte
@@ -31,59 +33,127 @@ func TestParseJSON(t *testing.T) {
 			},
 			expectedTimestamp: time.Unix(1257894000, 0),
 		},
+		{
+			name: "Success: Metrics and timestamp",
+			body: []byte(`{"timestamp":1257894000,"temp":12.34,"preassure":10.23,"count":9}`),
+			expectedMetrics: map[string]float64{
+				"temp":      12.34,
+				"preassure": 10.23,
+				"count":     9,
+			},
+			expectedTimestamp: time.Unix(1257894000, 0),
+		},
+		{
+			name: "Success: Tags and timestamp",
+			body: []byte(`{"timestamp":1257894000,"name":"test_name","tag":"test_tag"}`),
+			expectedTags: map[string]string{
+				"tag":  "test_tag",
+				"name": "test_name",
+			},
+			expectedTimestamp: time.Unix(1257894000, 0),
+		},
+		{
+			name: "Success: Only metrics",
+			body: []byte(`{"temp":12.34,"preassure":10.23,"count":9}`),
+			expectedMetrics: map[string]float64{
+				"temp":      12.34,
+				"preassure": 10.23,
+				"count":     9,
+			},
+		},
+		{
+			name: "Success: Only tags",
+			body: []byte(`{"name":"test_name","tag":"test_tag"}`),
+			expectedTags: map[string]string{
+				"tag":  "test_tag",
+				"name": "test_name",
+			},
+		},
+		{
+			name:          "Failure: invalid JSON body",
+			body:          []byte(`this is not JSON`),
+			expectedError: true,
+		},
+		{
+			name:          "Failure: invalid JSON value",
+			body:          []byte(`{"invalid_field":0x88}`),
+			expectedError: true,
+		},
 	}
 
 	var log = logrus.New()
+	var testFailed bool
 
+testCaseLoop:
 	for _, c := range cases {
 		t.Logf("starting test case: %s", c.name)
 
 		metrics, tags, timestamp, err := parseJSON(c.body, logrus.NewEntry(log))
 		if err != nil {
 			if !c.expectedError {
-				t.Fatal(errors.Wrap(err, "unexpected error"))
-			} else {
-				continue
+				t.Error(errors.Wrap(err, "unexpected error"))
+				testFailed = true
 			}
+
+			continue
 		}
 
 		if len(c.expectedMetrics) != len(metrics) {
-			t.Fatalf("expected no. of metrics %d, got %d", len(c.expectedMetrics), len(metrics))
+			t.Errorf("expected no. of metrics %d, got %d", len(c.expectedMetrics), len(metrics))
+			testFailed = true
+			continue
 		}
 
 		for metricKey, metricVal := range metrics {
 			expectedVal, ok := c.expectedMetrics[metricKey]
 			if !ok {
-				t.Fatalf("unexpected metric: %s", metricKey)
+				t.Errorf("unexpected metric: %s", metricKey)
+				testFailed = true
+				continue testCaseLoop
 			}
 
 			if expectedVal != metricVal {
-				t.Fatalf("unexpected value for key: %s, expected metric: %g, got: %g", metricKey, expectedVal, metricVal)
+				t.Errorf("unexpected value for key: %s, expected metric: %g, got: %g", metricKey, expectedVal, metricVal)
+				testFailed = true
+				continue testCaseLoop
 			}
 		}
 
 		if len(c.expectedTags) != len(tags) {
-			t.Fatalf("expected no. of tags %d, got %d", len(c.expectedTags), len(tags))
+			t.Errorf("expected no. of tags %d, got %d", len(c.expectedTags), len(tags))
+			testFailed = true
+			continue
 		}
 
 		for tagKey, tagVal := range tags {
 			expectedVal, ok := c.expectedTags[tagKey]
 			if !ok {
-				t.Fatalf("unexpected tag: %s", tagKey)
+				t.Errorf("unexpected tag: %s", tagKey)
+				testFailed = true
+				continue testCaseLoop
 			}
 
 			if expectedVal != tagVal {
-				t.Fatalf("unexpected value for key: %s, expected tag: %s, got: %s", tagKey, expectedVal, tagVal)
+				t.Errorf("unexpected value for key: %s, expected tag: %s, got: %s", tagKey, expectedVal, tagVal)
+				testFailed = true
+				continue testCaseLoop
 			}
 		}
 
 		if c.expectedTimestamp != timestamp {
 			t.Fatalf("expected timestamp: %d got: %d", c.expectedTimestamp.Unix(), timestamp.Unix())
+			testFailed = true
 		}
+	}
+
+	if testFailed {
+		t.Error("failure in test cases")
 	}
 }
 
 func TestParseCSV(t *testing.T) {
+	t.Parallel()
+
 	cases := []struct {
 		name              string
 		body              []byte
@@ -108,10 +178,75 @@ func TestParseCSV(t *testing.T) {
 			},
 			expectedTimestamp: time.Unix(1257894000, 0),
 		},
+		{
+			name: "Success: Metrics and timestamp",
+			body: []byte(
+				"timestamp;temp;preassure;count\n1257894000;12.34;10.23;9",
+			),
+			expectedMetrics: map[string]float64{
+				"temp":      12.34,
+				"preassure": 10.23,
+				"count":     9,
+			},
+			expectedTimestamp: time.Unix(1257894000, 0),
+		},
+		{
+			name: "Success: Tags and timestamp",
+			body: []byte(
+				"timestamp;name;tag\n1257894000;test_name;test_tag",
+			),
+			expectedTags: map[string]string{
+				"tag":  "test_tag",
+				"name": "test_name",
+			},
+			expectedTimestamp: time.Unix(1257894000, 0),
+		},
+		{
+			name: "Success: Only metrics",
+			body: []byte(
+				"temp;preassure;count\n12.34;10.23;9",
+			),
+			expectedMetrics: map[string]float64{
+				"temp":      12.34,
+				"preassure": 10.23,
+				"count":     9,
+			},
+		},
+		{
+			name: "Success: Only tags",
+			body: []byte(
+				"name;tag\ntest_name;test_tag",
+			),
+			expectedTags: map[string]string{
+				"tag":  "test_tag",
+				"name": "test_name",
+			},
+		},
+		{
+			name: "Success: HEX CSV value, dosen't work in JSON",
+			body: []byte("invalid_field\n0x88"),
+			expectedTags: map[string]string{
+				"invalid_field": "0x88",
+			},
+		},
+		{
+			name: "Failure: Headers only",
+			body: []byte(
+				"timestamp;name;tag;temp;preassure;count",
+			),
+			expectedError: true,
+		},
+		{
+			name:          "Failure: invalid CSV body",
+			body:          []byte(`this is not CSV`),
+			expectedError: true,
+		},
 	}
 
 	var log = logrus.New()
+	var testFailed bool
 
+testCaseLoop:
 	for _, c := range cases {
 		t.Logf("starting test case: %s", c.name)
 
@@ -119,23 +254,30 @@ func TestParseCSV(t *testing.T) {
 		if err != nil {
 			if !c.expectedError {
 				t.Fatal(errors.Wrap(err, "unexpected error"))
-			} else {
-				continue
+				testFailed = true
 			}
+
+			continue
 		}
 
 		if len(c.expectedMetrics) != len(metrics) {
 			t.Fatalf("expected no. of metrics %d, got %d", len(c.expectedMetrics), len(metrics))
+			testFailed = true
+			continue
 		}
 
 		for metricKey, metricVal := range metrics {
 			expectedVal, ok := c.expectedMetrics[metricKey]
 			if !ok {
 				t.Fatalf("unexpected metric: %s", metricKey)
+				testFailed = true
+				continue testCaseLoop
 			}
 
 			if expectedVal != metricVal {
 				t.Fatalf("unexpected value for key: %s, expected metric: %g, got: %g", metricKey, expectedVal, metricVal)
+				testFailed = true
+				continue testCaseLoop
 			}
 		}
 
@@ -147,15 +289,24 @@ func TestParseCSV(t *testing.T) {
 			expectedVal, ok := c.expectedTags[tagKey]
 			if !ok {
 				t.Fatalf("unexpected tag: %s", tagKey)
+				testFailed = true
+				continue testCaseLoop
 			}
 
 			if expectedVal != tagVal {
 				t.Fatalf("unexpected value for key: %s, expected tag: %s, got: %s", tagKey, expectedVal, tagVal)
+				testFailed = true
+				continue testCaseLoop
 			}
 		}
 
 		if c.expectedTimestamp != timestamp {
 			t.Fatalf("expected timestamp: %d got: %d", c.expectedTimestamp.Unix(), timestamp.Unix())
+			testFailed = true
 		}
+	}
+
+	if testFailed {
+		t.Error("failure in test cases")
 	}
 }
